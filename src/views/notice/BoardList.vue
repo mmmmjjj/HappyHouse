c2452ad6309b3e8d62d181c913a27abcd05fc75b
이호진 <roycmlhj@gmail.com>
2021-11-20 21:35:00 +0900

    upload

---
+++ src/views/notice/BoardList.vue
@@ -0,0 +1,452 @@
+<template>
+  <div class="wrapper">
+    <parallax class="page-header header-filter" :style="headerStyle">
+      <div class="md-layout">
+        <div class="md-layout-item">
+          <div class="image-wrapper">
+            <img :src="leaf4" alt="leaf4" class="leaf4" v-show="leafShow" />
+            <img :src="leaf3" alt="leaf3" class="leaf3" v-show="leafShow" />
+            <img :src="leaf2" alt="leaf2" class="leaf2" v-show="leafShow" />
+            <img :src="leaf1" alt="leaf1" class="leaf1" v-show="leafShow" />
+            <div class="brand">
+              <h1>Vue Material Kit</h1>
+              <h3>공지사항을 확인하세요</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="section section-basic">
+        <div class="container">
+          <div class="title">
+            <h2>Basic Elements</h2>
+          </div>
+          <basic-elements></basic-elements>
+        </div>
+        <!-- -->
+        <b-container class="bv-example-row mt-3">
+          <b-row>
+            <b-col>
+              <b-alert show><h3>공지사항</h3></b-alert>
+            </b-col>
+          </b-row>
+          <b-row class="mb-1">
+            <b-col class="text-right">
+              <!-- <b-button
+                variant="outline-primary"
+                @click="moveWrite()"
+                v-if="userInfo.admin == 1"
+                >글쓰기</b-button
+              > -->
+              <b-button variant="outline-primary" @click="check"
+                >글쓰기</b-button
+              >
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col v-if="articles.length">
+              <paginated-list :list-array="articles" />
+            </b-col>
+            <b-col v-else class="text-center"
+              >작성된 공지사항이 없습니다.</b-col
+            >
+          </b-row>
+        </b-container>
+        <!-- -->
+      </div>
+      <div class="section section-navbars">
+        <div class="container">
+          <small-navigation></small-navigation>
+        </div>
+        <navigation></navigation>
+      </div>
+      <!-- <div class="section section-tabs">
+        <div class="container">
+          <tabs></tabs>
+        </div>
+      </div> -->
+      <div class="section section-white">
+        <div class="container">
+          <nav-pills></nav-pills>
+        </div>
+      </div>
+      <div class="section section-notifications">
+        <div class="container">
+          <div class="title">
+            <h3>Notifications</h3>
+          </div>
+        </div>
+        <notifications></notifications>
+      </div>
+      <div class="section">
+        <div class="container">
+          <typography-images></typography-images>
+        </div>
+      </div>
+      <div class="section section-javascript">
+        <div class="container">
+          <javascript-components></javascript-components>
+        </div>
+      </div>
+      <div class="section">
+        <div class="container text-center">
+          <div class="md-layout">
+            <div
+              class="md-layout-item md-size-66 md-xsmall-size-100 ml-auto mr-auto text-center"
+            >
+              <h2>Completed with examples</h2>
+              <h4>
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. More importantly, looking at them will give you a picture of
+                what you can built with this powerful kit.
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-signup page-header" :style="signupImage">
+        <div class="container">
+          <div class="md-layout">
+            <div
+              class="md-layout-item md-size-33 md-medium-size-40 md-small-size-50 md-xsmall-size-70 mx-auto text-center"
+            >
+              <login-card header-color="green">
+                <h4 slot="title" class="card-title">Login</h4>
+                <md-button
+                  slot="buttons"
+                  href="javascript:void(0)"
+                  class="md-just-icon md-simple md-white"
+                >
+                  <i class="fab fa-facebook-square"></i>
+                </md-button>
+                <md-button
+                  slot="buttons"
+                  href="javascript:void(0)"
+                  class="md-just-icon md-simple md-white"
+                >
+                  <i class="fab fa-twitter"></i>
+                </md-button>
+                <md-button
+                  slot="buttons"
+                  href="javascript:void(0)"
+                  class="md-just-icon md-simple md-white"
+                >
+                  <i class="fab fa-google-plus-g"></i>
+                </md-button>
+                <p slot="description" class="description">Or Be Classical</p>
+                <md-field class="md-form-group" slot="inputs">
+                  <md-icon>face</md-icon>
+                  <label>First Name...</label>
+                  <md-input v-model="firstname"></md-input>
+                </md-field>
+                <md-field class="md-form-group" slot="inputs">
+                  <md-icon>email</md-icon>
+                  <label>Email...</label>
+                  <md-input v-model="email" type="email"></md-input>
+                </md-field>
+                <md-field class="md-form-group" slot="inputs">
+                  <md-icon>lock_outline</md-icon>
+                  <label>Password...</label>
+                  <md-input v-model="password"></md-input>
+                </md-field>
+                <md-button slot="footer" class="md-simple md-success md-lg">
+                  Get Started
+                </md-button>
+              </login-card>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="md-layout">
+        <div class="md-layout-item text-center">
+          <md-button href="#/login" class="md-simple md-success md-lg"
+            >View Login Page</md-button
+          >
+        </div>
+      </div>
+      <div class="section section-examples">
+        <div class="container-fluid text-center">
+          <div class="md-layout">
+            <div class="md-layout-item">
+              <a href="#/landing" target="_blank">
+                <img
+                  :src="landing"
+                  alt="Rounded Image"
+                  class="img-raised rounded img-fluid"
+                />
+              </a>
+              <md-button href="#/landing" class="md-simple md-success md-lg"
+                >View Landing Page</md-button
+              >
+            </div>
+            <div class="md-layout-item">
+              <a href="#/profile" target="_blank">
+                <img
+                  :src="profile"
+                  alt="Rounded Image"
+                  class="img-raised rounded img-fluid"
+                />
+              </a>
+              <md-button href="#/profile" class="md-simple md-success md-lg"
+                >View Profile Page</md-button
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-download" id="downloadSection">
+        <div class="container">
+          <div class="md-layout text-center">
+            <div class="md-layout-item md-size-66 md-small-size-100 mx-auto">
+              <h2>Do you love this UI Kit?</h2>
+              <h4>
+                Cause if you do, it can be yours for FREE. Hit the buttons below
+                to navigate to our website where you can find the kit. Our
+                friends from
+                <a
+                  href="https://themeisle.com/?utm_campaign=mkfree-hestia&amp;utm_source=creativetim&amp;utm_medium=website"
+                  target="_blank"
+                  >ThemeIsle</a
+                >
+                created a Wordpress Theme which can be also downloaded for free.
+                Start a new project or give an old Bootstrap project a new look!
+              </h4>
+            </div>
+            <div class="md-layout-item md-size-50 md-small-size-100 mx-auto">
+              <md-button
+                href="https://www.creative-tim.com/product/material-kit"
+                target="_blank"
+                class="md-success md-lg"
+                ><i class="fab fa-html5"></i> Free HTML Download</md-button
+              >
+              <md-button
+                href="https://themeisle.com/themes/hestia/?utm_campaign=mkfree-hestia&amp;utm_source=creativetim&amp;utm_medium=website"
+                class="md-success md-lg"
+                target="_blank"
+                ><i class="fab fa-wordpress"></i> Wordpress Theme</md-button
+              >
+            </div>
+          </div>
+          <br /><br />
+
+          <div class="md-layout text-center">
+            <div class="md-layout-item md-size-66 md-small-size-100 mx-auto">
+              <h2>Want more?</h2>
+              <h4>
+                We've just launched
+                <a
+                  href="https://www.creative-tim.com/product/vue-material-dashboard-pro?ref=utp-mk-demos"
+                  target="_blank"
+                  >Vue Material Dashboard PRO</a
+                >
+                and
+                <a
+                  href="https://www.creative-tim.com/product/vue-material-kit-pro?ref=utp-vmk-demos"
+                  target="_blank"
+                  >Vue Material Kit PRO</a
+                >. Every product has a huge number of components, sections and
+                example pages. Start Your Development With A Badass Bootstrap UI
+                Kit inspired by Material Design.
+              </h4>
+            </div>
+            <div class="md-layout-item md-size-50 md-small-size-100 mx-auto">
+              <div class="btn-container">
+                <md-button
+                  href="https://www.creative-tim.com/product/vue-material-kit-pro?ref=utp-vmk-demos"
+                  class="md-button md-success md-lg md-upgrade"
+                  target="_blank"
+                  ><md-icon>unarchive</md-icon>Vue Material Kit PRO</md-button
+                >
+                <md-button
+                  href="https://www.creative-tim.com/product/vue-material-dashboard-pro?ref=utp-vmd-demos"
+                  class="md-button md-success md-lg md-upgrade"
+                  target="_blank"
+                  ><md-icon>unarchive</md-icon>Vue Material Dashboard
+                  PRO</md-button
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="sharing-area text-center">
+            <div class="md-layout">
+              <div class="md-layout-item">
+                <h3>Thank you for supporting us!</h3>
+              </div>
+            </div>
+
+            <md-button class="md-twitter"
+              ><i class="fab fa-twitter"></i>Tweet</md-button
+            >
+            <md-button class="md-facebook"
+              ><i class="fab fa-facebook-square"></i> Share</md-button
+            >
+            <md-button class="md-google"
+              ><i class="fab fa-google-plus"></i> Share</md-button
+            >
+            <md-button class="md-github"
+              ><i class="fab fa-github"></i> Star</md-button
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import BasicElements from "./components/BasicElementsSection";
+// import Navigation from "./components/NavigationSection";
+// import SmallNavigation from "./components/SmallNavigationSection";
+// import Tabs from "./components/TabsSection";
+// import NavPills from "./components/NavPillsSection";
+// import Notifications from "./components/NotificationsSection";
+// import TypographyImages from "./components/TypographyImagesSection";
+// import JavascriptComponents from "./components/JavascriptComponentsSection";
+// import { LoginCard } from "@/components";
+import { listArticle } from "@/api/board.js";
+import PaginatedList from "@/views/notice/child/PaginatedList";
+import { mapActions, mapMutations, mapState } from "vuex";
+
+const memberStore = "memberStore";
+export default {
+  components: {
+    // BasicElements,
+    // Navigation,
+    // SmallNavigation,
+    // Tabs,
+    // NavPills,
+    // Notifications,
+    // TypographyImages,
+    // JavascriptComponents,
+    // LoginCard,
+    PaginatedList,
+  },
+  name: "boardlist",
+  bodyClass: "index-page",
+  props: {
+    image: {
+      type: String,
+      default: require("@/assets/img/vue-mk-header.jpg"),
+    },
+    leaf4: {
+      type: String,
+      default: require("@/assets/img/leaf4.png"),
+    },
+    leaf3: {
+      type: String,
+      default: require("@/assets/img/leaf3.png"),
+    },
+    leaf2: {
+      type: String,
+      default: require("@/assets/img/leaf2.png"),
+    },
+    leaf1: {
+      type: String,
+      default: require("@/assets/img/leaf1.png"),
+    },
+    signup: {
+      type: String,
+      default: require("@/assets/img/city.jpg"),
+    },
+    landing: {
+      type: String,
+      default: require("@/assets/img/landing.jpg"),
+    },
+    profile: {
+      type: String,
+      default: require("@/assets/img/profile.jpg"),
+    },
+  },
+  data() {
+    return {
+      user: {
+        userid: "",
+        username: "",
+        userpwd: "",
+        pwdchk: "",
+        email: "",
+        region: "",
+      },
+      firstname: null,
+      email: null,
+      password: null,
+      leafShow: false,
+      articles: [],
+    };
+  },
+  methods: {
+    ...mapActions(memberStore, ["getUserInfo"]),
+    ...mapMutations(memberStore, ["SET_IS_LOGIN", "SET_USER_INFO"]),
+    leafActive() {
+      if (window.innerWidth < 768) {
+        this.leafShow = false;
+      } else {
+        this.leafShow = true;
+      }
+    },
+    check() {
+      let token = sessionStorage.getItem("access-token");
+      if (this.getUserInfo(token)) {
+        console.log("sucess");
+        console.log(this.user.admin);
+      }
+    },
+  },
+  created() {
+    this.user = null;
+    this.user = this.userInfo;
+    let param = {
+      pg: 1,
+      spp: 20,
+      key: null,
+      word: null,
+    };
+    listArticle(
+      param,
+      (response) => {
+        this.articles = response.data;
+        console.log(this.articles);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  computed: {
+    ...mapState(memberStore, ["isLogin", "userInfo"]),
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.image})`,
+      };
+    },
+    signupImage() {
+      return {
+        backgroundImage: `url(${this.signup})`,
+      };
+    },
+  },
+  mounted() {
+    this.leafActive();
+    window.addEventListener("resize", this.leafActive);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.leafActive);
+  },
+};
+</script>
+<style lang="scss">
+.section-download {
+  .md-button + .md-button {
+    margin-left: 5px;
+  }
+}
+
+@media all and (min-width: 991px) {
+  .btn-container {
+    display: flex;
+  }
+}
+</style>
---
+++ src/views/notice/child/PaginatedList.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <b-table-simple hover responsive>
+      <b-thead head-variant="dark">
+        <b-tr>
+          <b-th>글번호</b-th>
+          <b-th>제목</b-th>
+          <b-th>조회수</b-th>
+          <b-th>작성자</b-th>
+          <b-th>작성일</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tr v-for="p in paginatedData" :key="p.no">
+        <b-td>{{ p.articleno }}</b-td>
+        <b-th class="text-left">
+          <router-link
+            :to="{ name: 'BoardView', params: { articleno: p.articleno } }"
+            >{{ p.subject }}</router-link
+          ></b-th
+        >
+        <b-td>{{ p.hit }}</b-td>
+        <b-td>{{ p.userid }}</b-td>
+        <b-td>{{ p.regtime }}</b-td>
+      </b-tr>
+    </b-table-simple>
+    <div class="btn-cover">
+      <button :disabled="pageNum === 0" @click="prevPage" class="page-btn">
+        이전
+      </button>
+      <span class="page-count">{{ pageNum + 1 }} / {{ pageCount }} 페이지</span>
+      <button
+        :disabled="pageNum >= pageCount - 1"
+        @click="nextPage"
+        class="page-btn"
+      >
+        다음
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "paginated-list",
+  data() {
+    return {
+      pageNum: 0,
+    };
+  },
+  props: {
+    listArray: {
+      type: Array,
+      required: true,
+    },
+    pageSize: {
+      type: Number,
+      required: false,
+      default: 10,
+    },
+  },
+  methods: {
+    nextPage() {
+      this.pageNum += 1;
+    },
+    prevPage() {
+      this.pageNum -= 1;
+    },
+  },
+  computed: {
+    pageCount() {
+      let listLeng = this.listArray.length,
+        listSize = this.pageSize,
+        page = Math.floor(listLeng / listSize);
+      if (listLeng % listSize > 0) page += 1;
+
+      /*
+      아니면 page = Math.floor((listLeng - 1) / listSize) + 1;
+      이런식으로 if 문 없이 고칠 수도 있다!
+      */
+      return page;
+    },
+    paginatedData() {
+      const start = this.pageNum * this.pageSize,
+        end = start + this.pageSize;
+      return this.listArray.slice(start, end);
+    },
+  },
+};
+</script>
+<style>
+.btn-cover {
+  margin-top: 1.5rem;
+  text-align: center;
+}
+.btn-cover .page-btn {
+  width: 5rem;
+  height: 2rem;
+  letter-spacing: 0.5px;
+}
+.btn-cover .page-count {
+  padding: 0 1rem;
+}
+</style>
