478c822ec1077db1656017fd0b6fdb87bd802566
mmmmjjj <myungju25@naver.com>
2021-11-22 02:10:33 +0900

    SignUp completed!

--- src/views/user/SignUp.vue
+++ src/views/user/SignUp.vue
@@ -9,19 +9,27 @@
             <login-card header-color="green">
               <h4 slot="title" class="card-title">SignUp</h4>
               <p slot="description" class="description">Welcome!</p>
-              <md-field class="md-form-group" slot="inputs">
+              <md-field :class="idCheck" slot="inputs">
                 <md-icon>face</md-icon>
-                <label>아이디를 입력하세요...</label>
-                <md-input ref="userid" v-model="user.userid"></md-input>
+                <label>{{ idtxt }}</label>
+                <md-input
+                  ref="userid"
+                  v-model="user.userid"
+                  @keyup="idPress"
+                ></md-input>
               </md-field>
               <md-field class="md-form-group" slot="inputs">
                 <md-icon>face</md-icon>
-                <label>이름을 입력하세요...</label>
-                <md-input ref="username" v-model="user.username"></md-input>
+                <label>{{ nametxt }}</label>
+                <md-input
+                  ref="username"
+                  v-model="user.username"
+                  @keyup="Press"
+                ></md-input>
               </md-field>
               <md-field class="md-form-group" slot="inputs">
                 <md-icon>lock_outline</md-icon>
-                <label>비밀번호를 입력하세요...</label>
+                <label>{{ pwdtxt }}</label>
                 <md-input
                   ref="userpwd"
                   v-model="user.userpwd"
@@ -31,7 +39,7 @@
               </md-field>
               <md-field :class="pwdCheck" slot="inputs">
                 <md-icon>lock_outline</md-icon>
-                <label>{{ pwdtxt }}</label>
+                <label>{{ pwdchktxt }}</label>
                 <md-input
                   ref="pwdchk"
                   v-model="user.pwdchk"
@@ -41,14 +49,22 @@
               </md-field>
               <md-field class="md-form-group" slot="inputs">
                 <md-icon>mail</md-icon>
-                <label>이메일을 입력하세요...</label>
-                <md-input ref="email" v-model="user.email"></md-input>
+                <label>{{ emailtxt }}</label>
+                <md-input
+                  ref="email"
+                  v-model="user.email"
+                  @keyup="Press"
+                ></md-input>
               </md-field>
 
               <md-field class="md-form-group" slot="inputs">
                 <md-icon>home</md-icon>
-                <label>거주지를 입력하세요...</label>
-                <md-input ref="region" v-model="user.region"></md-input>
+                <label>{{ regiontxt }}</label>
+                <md-input
+                  ref="region"
+                  v-model="user.region"
+                  @keyup="Press"
+                ></md-input>
               </md-field>
               <md-button
                 slot="footer"
@@ -63,48 +79,17 @@
       </div>
     </div>
   </div>
-  <!--
-                  <b-button type="submit" variant="primary" class="m-1"
-                    >회원가입</b-button
-                  >
-                  <b-button type="reset" variant="success" class="m-1"
-                    >초기화</b-button
-                  >
-                </b-form>
-              </b-card>
-            </b-col>
-            <b-col></b-col>
-          </b-row>
-        </b-container> -->
-  <!-- -->
 </template>
 
 <script>
 import { mapState, mapActions, mapMutations } from "vuex";
 import http from "@/util/http-common";
 import { LoginCard } from "@/components";
-// import BasicElements from "./components/BasicElementsSection";
-// import Navigation from "./components/NavigationSection";
-// import SmallNavigation from "./components/SmallNavigationSection";
-// import Tabs from "./components/TabsSection";
-// import NavPills from "./components/NavPillsSection";
-// import Notifications from "./components/NotificationsSection";
-// import TypographyImages from "./components/TypographyImagesSection";
-// import JavascriptComponents from "./components/JavascriptComponentsSection";
-// import { LoginCard } from "@/components";
 
 const houseStore = "houseStore";
 
 export default {
   components: {
-    // BasicElements,
-    // Navigation,
-    // SmallNavigation,
-    // Tabs,
-    // NavPills,
-    // Notifications,
-    // TypographyImages,
-    // JavascriptComponents,
     LoginCard,
   },
   name: "signup",
@@ -153,10 +138,14 @@ export default {
         email: "",
         region: "",
       },
-      sidoCode: null,
-      gugunCode: null,
       isCheckedPwd: null,
-      pwdtxt: "다시 한번 입력해주세요...",
+      idchk: null,
+      idtxt: "아이디를 입력하세요.",
+      nametxt: "이름을 입력하세요.",
+      emailtxt: "이메일을 입력하세요.",
+      pwdtxt: "비밀번호를 입력하세요.",
+      pwdchktxt: "다시 한번 입력해주세요.",
+      regiontxt: "거주지를 입력하세요.",
     };
   },
   created() {
@@ -201,21 +190,18 @@ export default {
         (err = false),
         this.$refs.region.$el.focus());
       err &&
+        this.idchk == false &&
+        ((msg = "아이디를 확인해주세요"),
+        (err = false),
+        this.$refs.userid.$el.focus());
+      err &&
         this.isCheckedPwd == false &&
         ((msg = "비밀번호를 확인해주세요"),
         (err = false),
         this.$refs.pwdchk.$el.focus());
+
       if (!err) alert(msg);
       else this.registMember();
-    },
-    PasswordCheck() {
-      if (this.user.userpwd == this.user.pwdchk) {
-        alert("비밀번호 확인완료");
-        this.isCheckedPwd = true;
-      } else {
-        alert("비밀번호를 확인해주세요");
-        this.$refs.pwdchk.$el.focus();
-      }
     },
     registMember() {
       http
@@ -243,20 +229,47 @@ export default {
     keyPress() {
       let pwdchk = this.$refs.pwdchk.$options.propsData.value;
       let pwd = this.$refs.userpwd.$options.propsData.value;
-      console.log(pwdchk);
+      if (pwd != null) this.pwdtxt = null;
       if (pwdchk == null) this.isCheckedPwd = null;
       else if (pwd == pwdchk) {
         this.isCheckedPwd = true;
-        this.pwdtxt = "사용 가능합니다.";
+        this.pwdchktxt = "사용 가능합니다.";
       }
       if (pwd != pwdchk && pwdchk != null) {
         this.isCheckedPwd = false;
-        this.pwdtxt = "비밀번호가 올바르지 않습니다.";
+        this.pwdchktxt = "비밀번호가 올바르지 않습니다.";
       }
     },
-  },
-  watch: {
-    userpwd: {},
+    idPress() {
+      let id = this.$refs.userid.$options.propsData.value;
+      if (id == "") {
+        this.idtxt = "아이디를 입력하세요.";
+        return;
+      }
+      http
+        .get(`/user/idcheck/${this.$refs.userid.$options.propsData.value}`)
+        .then(({ data }) => {
+          if (data == 0) {
+            this.idchk = true;
+            this.idtxt = "이 아이디는 사용 가능합니다.";
+          } else if (data == 1) {
+            this.idchk = false;
+            this.idtxt = "이미 사용 중인 아이디입니다.";
+          }
+        });
+    },
+    Press() {
+      let name = this.$refs.username.$options.propsData.value;
+      let email = this.$refs.email.$options.propsData.value;
+      let region = this.$refs.region.$options.propsData.value;
+
+      if (name != "") this.nametxt = null;
+      else this.nametxt = "이름을 입력하세요.";
+      if (email != "") this.emailtxt = null;
+      else this.emailtxt = "이메일을 입력하세요.";
+      if (region != "") this.regiontxt = null;
+      else this.regiontxt = "거주지를 입력하세요.";
+    },
   },
   computed: {
     ...mapState(houseStore, ["sidos", "guguns"]),
@@ -271,11 +284,17 @@ export default {
       };
     },
     pwdCheck: function() {
-      console.log(this.isCheckedPwd);
       return {
         "md-form-group": this.isCheckedPwd == null,
         "md-error": this.isCheckedPwd == false,
         "md-valid": this.isCheckedPwd == true,
+      };
+    },
+    idCheck: function() {
+      return {
+        "md-form-group": this.idchk == null,
+        "md-error": this.idchk == false,
+        "md-valid": this.idchk == true,
       };
     },
   },
